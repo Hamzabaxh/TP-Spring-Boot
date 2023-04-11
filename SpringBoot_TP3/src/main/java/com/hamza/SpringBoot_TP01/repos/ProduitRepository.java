d146b8848ced05a301d8b3a25ed8ee89b593ad54
Hamzabaxh <127216152+Hamzabaxh@users.noreply.github.com>
2023-04-11 16:04:53 +0100

    Add files via upload

---
+++ SpringBoot_TP3/src/main/java/com/hamza/SpringBoot_TP01/repos/ProduitRepository.java
@@ -0,0 +1,10 @@
+package com.hamza.SpringBoot_TP01.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hamza.SpringBoot_TP01.entities.Produit;
+
+public interface ProduitRepository extends JpaRepository<Produit, Long> {
+	
+
+}
